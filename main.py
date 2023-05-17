31549c150ff19f685ee474712a3df386ff89a91a
Super-Ace <mr.timemark@gmail.com>
2023-05-17 22:41:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -50,4 +50,11 @@ say_hello()
 def plus(a, b):
     print(a + b)
 
+def tax_calc(money):
+    return money * 0.35
 
+def pay_tax(tax):
+    print("thank you for paying", tax)
+
+to_pay = tax_calc(150000000)
+pay_tax(to_pay)
