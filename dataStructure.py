38421fbb417bd6fa818047fb98f7daad18a5f52e
Super-Ace <mr.timemark@gmail.com>
2023-05-21 15:48:48 +0900

    Update dataStructure.py file

--- dataStructure.py
+++ dataStructure.py
@@ -16,4 +16,24 @@ name = "joung"
 #print(name.replace("o","😊"))
 print(name.endswith("o"))
 """
-print("Joung".endswith("o"))
+# print("Joung".endswith("o"))
+
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+# print(days_of_week.count("Wed"))
+# days_of_week.clear()
+# days_of_week.reverse()
+"""
+print(days_of_week)
+days_of_week.append("Sat")
+print(days_of_week)
+days_of_week.append("Sun")
+print(days_of_week)
+
+print(days_of_week)
+days_of_week.remove("Fri")
+print(days_of_week)
+
+"Mon, Tue, Wed"
+"""
+print(days_of_week[2])
