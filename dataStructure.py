54538a6a87e3d9a1b37e783ff41e704ae514111c
Super-Ace <mr.timemark@gmail.com>
2023-05-21 14:58:03 +0900

    Create dataStructure.py file

---
+++ dataStructure.py
@@ -0,0 +1,19 @@
+"""
+mon = "Mon"
+tue = "Tue"
+wed = "Wed"
+thu = "Thu"
+fri = "Fri"
+
+days_of_week = "Mon,Tue,Wed,Thu,Fri"
+print(days_of_week)
+
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+print(days_of_week)
+"""
+"""
+name = "joung"
+#print(name.replace("o","😊"))
+print(name.endswith("o"))
+"""
+print("Joung".endswith("o"))
