73d93f638011b0885e6d82f85dd91bc19dce31e2
Super-Ace <mr.timemark@gmail.com>
2024-01-30 20:12:19 +0900

    Update JobScraper1.py

--- JobScraper1.py
+++ JobScraper1.py
@@ -10,5 +10,13 @@ soup = BeautifulSoup(
     "html.parser",    
 )
 
-jobs = soup.find("section", class_="jobs").find_all("li")
-print(jobs)
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
+
+for job in jobs:
+    title = job.find("span", class_="title").text
+    # region = job.find("span", class_="region").text
+    company, position, region  = job.find_all("span", class_="company")
+    company = company.text
+    position = position.text
+    region = region.text
+    print(title, company, position, region, "-----\n")
