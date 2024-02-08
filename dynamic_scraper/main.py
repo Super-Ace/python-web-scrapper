90b94b4dda0b9f29abc55c04d8ee09d8f161e783
Super-Ace <mr.timemark@gmail.com>
2024-02-09 00:04:27 +0900

    Update main.py

--- dynamic_scraper/main.py
+++ dynamic_scraper/main.py
@@ -8,25 +8,26 @@ browser = p.chromium.launch(headless=False)
 
 page = browser.new_page()
 
-page.goto("https://www.wanted.co.kr/jobsfeed")
+page.goto("https://www.wanted.co.kr/search?query=flutter&tab=position")
+# page.goto("https://www.wanted.co.kr/jobsfeed")
 
-time.sleep(5)
+# time.sleep(5)
 
-page.click("button.Aside_searchButton__Xhqq3")
+# page.click("button.Aside_searchButton__Xhqq3")
 
-# page.locator("button.Aside_searchButton__Xhqq3").click()
+# # page.locator("button.Aside_searchButton__Xhqq3").click()
 
-time.sleep(5)
+# time.sleep(5)
 
-page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+# page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
 
-time.sleep(5)
+# time.sleep(5)
 
-page.keyboard.down("Enter")
+# page.keyboard.down("Enter")
 
-time.sleep(5)
+# time.sleep(5)
 
-page.click("a#search_tab_position")
+# page.click("a#search_tab_position")
 
 for x in range(5):
     time.sleep(5)
@@ -39,3 +40,25 @@ content = page.content()
 p.stop()
 
 soup = BeautifulSoup(content, "html.parser")
+
+jobs = soup.find_all("div", class_ = "JobCard_container__FqChn")
+
+jobs_db = []
+
+for job in jobs:
+    link = f"https://www.wanted.co.kr{job.find('a')['href']}"
+    title = job.find("JobCard_title__ddkwM")
+    company_name = job.find("span", class_="JobCard_companyName__vZMqJ").text
+    location = job.find("span", class_="JobCard_location__2EOr5").text
+    reward = job.find("span", class_="JobCard_reward__sdyHn").text
+    job = {
+        "title":title,
+        "company_name":company_name,
+        "location":location,
+        "reward":reward,
+        "link":link,
+    }
+    jobs_db.append(job)
+
+print(jobs_db)
+print(len(jobs_db))
