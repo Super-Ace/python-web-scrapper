8c7257b0d482a8058ba89dfc185c3c5d59067309
Super-Ace <mr.timemark@gmail.com>
2024-03-01 00:12:57 +0900

    update main4.py

--- main4.py
+++ main4.py
@@ -9,12 +9,19 @@ def home():
     # return "<h1>hey there!</h1><a href='/hello'>go to hello</a>"
     return render_template("home.html", name="nico")
 
+db = {
+}
+
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    indeed = extract_indeed_jobs(keyword)
-    wwr = extract_wwr_jobs(keyword)
-    jobs = indeed + wwr
+    if keyword in db:
+        jobs = db['python']
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = indeed + wwr
+        db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs = jobs)
 
 app.run("0.0.0.0")
