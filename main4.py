06a0f6ad9e04c884272f4d703540f8a70b366104
Super-Ace <mr.timemark@gmail.com>
2024-03-01 14:24:37 +0900

    update main4.py

--- main4.py
+++ main4.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
+
 
 app = Flask("JobScrapper")
 
@@ -15,13 +17,26 @@ db = {
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
+    # print(keyword)
+    if keyword == None:
+        return redirect("/")
     if keyword in db:
-        jobs = db['python']
+        jobs = db[keyword]
     else:
         indeed = extract_indeed_jobs(keyword)
         wwr = extract_wwr_jobs(keyword)
         jobs = indeed + wwr
         db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs = jobs)
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachmen=True)
 
 app.run("0.0.0.0")
